4005aeec0d494af56a74ea221098d90574ccdea6
johnnyb-consolidez <jbohorquez@consolidez.com.ve>
2025-10-27 14:53:10 -0400

    07 - Components App started

---
+++ 07-components-app/app/_layout.tsx
@@ -0,0 +1,52 @@
+import '@/global.css';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from '@react-navigation/native';
+import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import 'react-native-reanimated';
+
+import { useColorScheme } from '@/hooks/use-color-scheme';
+import { useThemeColor } from '@/hooks/use-theme-color';
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  const colorScheme = useColorScheme();
+  const backgroundColor = useThemeColor({}, 'background');
+
+  // const [loaded] = useFonts({
+  //   SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+  // });
+
+  // useEffect(() => {
+  //   if (loaded) {
+  //     SplashScreen.hideAsync();
+  //   }
+  // }, [loaded]);
+
+  // if (!loaded) {
+  //   return null;
+  // }
+
+  return (
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <Stack
+        screenOptions={{
+          headerShadowVisible: false,
+          contentStyle: {
+            backgroundColor: 'pink',
+          },
+          headerStyle: {
+            backgroundColor: 'pink',
+          },
+        }}
+      >
+        <Stack.Screen name="index" options={{ title: 'Inicio' }} />s
+      </Stack>
+    </ThemeProvider>
+  );
+}
---
+++ 07-components-app/app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native';
+
+const ComponentsApp = () => {
+  return (
+    <View>
+      <Text>ComponentssApp</Text>
+    </View>
+  );
+};
+export default ComponentsApp;
